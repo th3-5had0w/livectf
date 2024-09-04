9416f6bae6c0e2c21d841d77c628634ac1f3c7b0
CP04042K <shin24@DESKTOP-35MQRGN>
2024-09-05 00:20:40 +0700

    add more frontend stuff

---
+++ RELEASE/static/static/js/register.js
@@ -0,0 +1,30 @@
+const btn = document.querySelector('input[name="register"]');
+
+btn.onclick = async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementsByName("email")[0].value;
+    const username = document.getElementsByName("username")[0].value;
+    const password = document.getElementsByName("password")[0].value;
+
+    const data = `email=${email}&username=${username}&password=${password}`
+    
+    let  res = await fetch("/api/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+        },
+        body: data
+    });
+    res = await res.json();
+
+    var expires = (new Date(Date.now()+ 86400*1000)).toUTCString();
+
+    if (!res["is_error"]) {
+        document.cookie = `auth=${res.message};expires=${expires}`
+        window.location.replace("/");
+    } else {
+        alert(res["message"]);
+    }
+
+}
---
+++ RELEASE/static/static/js/sheep_center.js
@@ -0,0 +1,103 @@
+Array.from(document.querySelectorAll(".del-btn")).map(btn => {
+    btn.onclick = async (e) => {
+        const userId = e.target.getAttribute("data-userid");
+        let res = await fetch("/api/user/"+userId, {
+            method: "DELETE",
+            credentials: "include",
+            mode: "cors"
+        });
+
+        res = await res.json();
+
+        if (res.is_error) {
+            alert(res.message);
+        } else {
+            location.reload();
+        }
+    }
+})
+
+Array.from(document.querySelectorAll(".ban-btn")).map(btn => {
+    btn.onclick = async (e) => {
+        alert("Banning feature is comming up soon...");
+    }
+})
+
+document.querySelector("#upload-challenge").addEventListener("click", async (e) => {
+    const data = new FormData(document.querySelector(".challenge-upload-form"));
+
+    e.preventDefault();
+    
+    let parsedStartTime = new Date(document.querySelector("#start-date").value + "T" + document.querySelector("#start-time").value + "Z");
+    let parsedEndTime = new Date(document.querySelector("#end-date").value + "T" + document.querySelector("#end-time").value + "Z");
+
+    parsedStartTime = Math.floor(parsedStartTime.getTime() / 1000);
+    parsedEndTime = Math.floor(parsedEndTime.getTime() / 1000);
+
+    let result = await fetch("/api/challenge-upload", {
+        method: "POST",
+        mode: "cors",
+        credentials: "include",
+        body: data,
+        headers: {
+            "X-start": parsedStartTime,
+            "X-end": parsedEndTime
+        }
+    });
+
+    result = await result.json();
+
+    if (!result.is_error) {
+        alert(result.message);
+        location.reload();
+    } else {
+        alert(result.message);
+    }
+})
+
+document.querySelector("#schedule-challenge").addEventListener("click", async (e) => {
+    e.preventDefault();
+    
+    let challenge_name = document.querySelector("#challenge-name").value
+    let parsedStartTime = new Date(document.querySelector("#start-date2").value + "T" + document.querySelector("#start-time2").value + "Z");
+    let parsedEndTime = new Date(document.querySelector("#end-date2").value + "T" + document.querySelector("#end-time2").value + "Z");
+
+    parsedStartTime = Math.floor(parsedStartTime.getTime() / 1000);
+    parsedEndTime = Math.floor(parsedEndTime.getTime() / 1000);
+
+    let result = await fetch(`/api/${challenge_name}/deploy`, {
+        method: "POST",
+        mode: "cors",
+        credentials: "include",
+        headers: {
+            "X-start": parsedStartTime,
+            "X-end": parsedEndTime
+        }
+    });
+
+    result = await result.json();
+
+    if (!result.is_error) {
+        alert(result.message);
+        location.reload();
+    } else {
+        alert(result.message);
+    }
+});
+
+document.querySelector("#stop-btn").addEventListener("click", async (e) => {
+    const challenge_name = e.target.getAttribute("data-challengeid");
+    let res = await fetch(`/api/${challenge_name}/destroy`, {
+        method: "POST",
+        credentials: "include",
+        mode: "cors"
+    });
+
+    res = await res.json();
+
+    if (res.is_error) {
+        alert(res.message);
+    } else {
+        location.reload();
+    }
+});
